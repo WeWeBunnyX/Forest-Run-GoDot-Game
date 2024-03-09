ee5540773a4bb9288494eee338a9d4a6080ed31e
Hashir Asad <hashirbinasad@gmail.com>
2024-03-09 22:33:55 +0500

    code working for run and idle animations

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,28 +1,48 @@
+
 extends CharacterBody2D
-
-
+@onready var animated_sprite_2d = $AnimatedSprite2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
+const gravity = 980.0
+enum State  { run , idle, jump , fall}
+var current_state
+func _ready():
+	current_state = State.idle
+	
+	
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	player_idle(delta)
+	player_run(delta)
+	#player_jump(delta)
+	player_falling(delta)
+	
 	move_and_slide()
+	player_animation()
+	
+func player_idle(delta):
+	if is_on_floor_only():
+		current_state = State.idle
+		
+		
+func player_falling(delta):
+	if !is_on_floor():
+		velocity.y = gravity*delta
+		
+		
+func player_run(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction and is_on_floor():
+		velocity.x = direction*SPEED
+		current_state = State.run
+	else:
+		velocity.x= move_toward(velocity.x, 0 , SPEED)
+		current_state = State.idle
+	
+	
+func player_animation():
+	if current_state == State.idle:
+		animated_sprite_2d.play("idle")
+	elif current_state == State.run:
+		animated_sprite_2d.play("run")
