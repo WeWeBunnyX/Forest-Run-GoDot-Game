02bf9847027e369b16e116b7becca9f2e4bfc3dc
Hashir Asad <hashirbinasad@gmail.com>
2024-03-29 19:25:38 +0500

    Update Player.gd

--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -1,6 +1,6 @@
 
 extends CharacterBody2D
-@onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var animated_sprite_2d = $AnimatedSprite2D  #1234
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
